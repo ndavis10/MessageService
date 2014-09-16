d4ac8f060a297c28a43dc52fffb949c89d2fd61c
viewt_000 <viewt_000@10.30.10.170>
2014-09-16 15:30:32 -0500

    Minor cleanup.

--- src/messageservice/ArrayReader.java
+++ src/messageservice/ArrayReader.java
@@ -14,7 +14,7 @@ public class ArrayReader implements MessageReader
 {
     private String[] strings =
     {"This is a test", "Please do not adjust your settings", "This has been a test"};
-    private final Random randomGenerator = new Random();
+    private final Random randomGenerator = new Random(System.nanoTime());
     
     public ArrayReader()
     {
@@ -32,7 +32,7 @@ public class ArrayReader implements MessageReader
         return strings[randomGenerator.nextInt(strings.length)];
     }
     
-    public void setStrings(String[] strings)
+    public final void setStrings(String[] strings)
     {
         this.strings = strings;
     }
